a1267dcf222886dc2dd4c4c1d8e0ba27e7946244
Julian Romana <you@example.com>
2019-03-19 09:07:54 +0100

    Playable with 2 players

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,28 @@
 import { runMain } from "module";
+let images = require('../assets/*.png');
 // Different variables
-let hand = document.querySelector("#hand");
-let tray = document.querySelector("#tray");
+
+// Player 1 variables 
+let hand1 = document.querySelector("#hand1");
+let tray1 = document.querySelector("#tray1");
+let hand_cards1 = document.querySelectorAll("#hand1 li");
+let tray_attack1= document.querySelectorAll("#tray1 figure")[0];
+let tray_defense1 = document.querySelectorAll("#tray1 figure")[1];
+let tray_bonus1 = document.querySelectorAll("#tray1 figure")[2];
+let tray_speed1 = document.querySelectorAll("#tray1 figure")[3];
+
+// Player 2 variables 
+let hand2 = document.querySelector("#hand2 ");
+let tray2 = document.querySelector("#tray2");
+let hand_cards2 = document.querySelectorAll("#hand2 li");
+let tray_attack2 = document.querySelectorAll("#tray2 figure")[0];
+let tray_defense2 = document.querySelectorAll("#tray2 figure")[1];
+let tray_bonus2 = document.querySelectorAll("#tray2 figure")[2];
+let tray_speed2 = document.querySelectorAll("#tray2 figure")[3];
+
 let speed = document.querySelector('h2')
-let hand_cards = document.querySelectorAll("#hand li");
-let tray_attack = document.querySelectorAll("#tray figure")[0];
-let tray_defense = document.querySelectorAll("#tray figure")[1];
-let tray_bonus = document.querySelectorAll("#tray figure")[2];
-let tray_speed= document.querySelectorAll("#tray figure")[3];
 let button = document.querySelector("button");
 
-// object for cards
 class Card {
   constructor(name, image, occurrence) {
     this.name = name;
@@ -18,25 +30,6 @@ class Card {
     this.occurence = occurrence;
   }
 }
-
-// Object who represents the player
-let player = {
-  hand: [],
-  speed: 0,
-  shuffleHand() {
-    for (let i = 0; i < hand_cards.length; i++) {
-      const li = hand_cards[i];
-      let randomNumber = Math.floor(Math.random() * cards.length);
-      let randomCard = cards[randomNumber]
-      let img = li.querySelector("img");
-      img.setAttribute("src", randomCard.image)
-      img.setAttribute("alt", randomCard.name)
-      this.hand.push(randomCard);
-      cards.splice(randomNumber, 1);
-    }
-  }
-};
-let images = require('../assets/*.png');
 // All the different  cards
 let accident = new Card("accident", images.accident, 3);
 let reparation = new Card("reparation", images.reparation, 6);
@@ -79,58 +72,112 @@ let cards = [
   panne_essence,
   essence
 ];
-
 cards = cards.reduce((memo, item) => {
   for (let i = 0; i < item.occurence; i++) {
     memo.push(item);
   }
   return memo;
 }, []);
+console.log(cards)
+// object for cards
+
+
+// Object who represents the player
+let player1 = {
+  hand1: [],
+  speed: 0,
+  shuffleHand() {
+    for (let i = 0; i < hand_cards1.length; i++) {
+      let randomNumber = Math.floor(Math.random() * cards.length);
+      let randomCard = cards[randomNumber]
+      let img = hand_cards1[i].querySelector("img");
+      img.setAttribute("src", randomCard.image)
+      img.setAttribute("alt", randomCard.name)
+      this.hand1.push(randomCard);
+      cards.splice(randomNumber, 1);
+    }
+  }
+};
+
+let player2 = {
+  hand2: [],
+  speed: 0,
+  shuffleHand() {
+    for (let i = 0; i < hand_cards2.length; i++) {
+      let randomNumber = Math.floor(Math.random() * cards.length);
+      let randomCard = cards[randomNumber]
+      let img = hand_cards2[i].querySelector("img");
+      img.setAttribute("src", randomCard.image)
+      img.setAttribute("alt", randomCard.name)
+      this.hand2.push(randomCard);
+      cards.splice(randomNumber, 1);
+    }
+  }
+};
+
 
 // Beggining of the game
 button.addEventListener("click", () => {
-  player.shuffleHand();
+  console.log('clicked')
+  player1.shuffleHand();
+  player2.shuffleHand();
   button.remove();
-  hand.classList.add("animation")
+  hand2.classList.add("animation__hand2")
+  hand1.classList.add("animation__hand1")
   setTimeout(function() {
-    tray.className = "tray"
-    hand.classList.remove("animation")
+    tray1.className = "tray1"
+    tray2.className = "tray2"
   }, 1200);
+  console.log(cards)
 });
 
-// Adding images to the tray
-for (let i = 0; i < hand_cards.length; i++) {
-  let cards = hand_cards[i];
-  cards.addEventListener('click', ()=>{
-    let innerImage = cards.querySelector('img')
+
+
+
+// Adding cards to the tray for player 1
+for (let i = 0; i < hand_cards1.length; i++) {
+  hand_cards1[i].addEventListener('click', ()=>{
+    let innerImage = hand_cards1[i].querySelector('img')
     let attribute = innerImage.getAttribute('alt')
     if (attribute === "50" || attribute === "25" || attribute === "75" || attribute === "100" || attribute === "200") {
-     if (tray_speed.children[1]) {
-       tray_speed.children[1].remove()
+     if (tray_speed1.children[1]) {
+       tray_speed1.children[1].remove()
      }
-      tray_speed.append(innerImage);
-      player.speed = player.speed + parseInt(attribute)
-      speed.innerHTML = `votre score: ` + player.speed
-      console.log(player.speed)
+      tray_speed1.append(innerImage);
+      player1.speed = player1.speed + parseInt(attribute)
+      speed.innerHTML = `votre score: ` + player1.speed
+      console.log('Player 1 speed: ' + player1.speed)
    }else if(attribute === "increvable" || attribute ===  "vehicule_prioritaire" || attribute ===  "as_volant" || attribute ===  "citerne"){
-      tray_bonus.append(innerImage)
+      tray_bonus1.append(innerImage)
    }else if (attribute === "feux_rouge" || attribute === "panne_essence" || attribute === "crevé" || attribute === "accident" || attribute === "limite_vitesse") {
-    tray_attack.append(innerImage)
+    tray_attack2.append(innerImage)
    } else if(attribute === "feux_vert" || attribute == "reparation" || attribute == "fin_vitesse" || attribute == "essence" || attribute == "roue_secours" ) {
-    tray_defense.append(innerImage)
+    tray_defense1.append(innerImage)
    }
   })
 }
-  
-
-
-
-
-
-
-
-
-
+// Adding cards to the tray for player  2
+for (let i = 0; i < hand_cards2.length; i++) {
+  hand_cards2[i].addEventListener('click', ()=>{
+    let innerImage = hand_cards2[i].querySelector('img')
+    let attribute = innerImage.getAttribute('alt')
+    if (attribute === "50" || attribute === "25" || attribute === "75" || attribute === "100" || attribute === "200") {
+     if (tray_speed2.children[1]) {
+       tray_speed2.children[1].remove()
+     }
+      tray_speed2.append(innerImage);
+      player2.speed = player2.speed + parseInt(attribute)
+      speed.innerHTML = `votre score: ` + player2.speed
+      console.log('Player 2 speed: ' + player2.speed)
+   }else if(attribute === "increvable" || attribute ===  "vehicule_prioritaire" || attribute ===  "as_volant" || attribute ===  "citerne"){
+      tray_bonus2.append(innerImage)
+   }else if (attribute === "feux_rouge" || attribute === "panne_essence" || attribute === "crevé" || attribute === "accident" || attribute === "limite_vitesse") {
+    tray_attack1.append(innerImage)
+   } else if(attribute === "feux_vert" || attribute == "reparation" || attribute == "fin_vitesse" || attribute == "essence" || attribute == "roue_secours" ) {
+    tray_defense2.append(innerImage)
+   }
+  })
+}
 
 
 
@@ -140,7 +187,7 @@ let movingImage = new Image();
 console.log(movingImage);
 for (let i = 0; i < hand_cards.length; i++) {
   hand_cards[i].addEventListener("dragstart", (e) => {
-    movingImage.src = `${player.hand[i].image}`;
+    movingImage.src = `${player1.hand2[i].image}`;
     e.dataTransfer.setDragImage(movingImage, 80, 80);
     console.log(movingImage)
     // Supprime le carte du deck quand elle a été déposée
